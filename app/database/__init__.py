e71d16e2fa112e9fe5b0d5f46a610d484fe91cb5
Igor Bondarenko <bondarenko200021@mail.ru>
2026-02-11 20:41:19 +0700

    make classes for work with DB

---
+++ app/database/__init__.py
@@ -0,0 +1,2 @@
+from app.database.db import db_constructor
+from app.database.base import Base
--- app/database/db.py
+++ app/database/db.py
@@ -1,3 +1,30 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from app.core.config import settings
+
+
 class DataBaseConstructor:
-    def __init__(self):
-        pass
+    def __init__(self, url: str, echo: bool):
+        self.engine = create_async_engine(
+            url=settings.url,
+            echo=settings.echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+    async def close_session(self):
+        if self.engine is not None:
+            return await self.engine.dispose()
+
+    async def get_session(self):
+        async with self.session_factory() as session:
+            yield session
+
+
+db_constructor = DataBaseConstructor(
+    url=settings.url,
+    echo=settings.echo,
+)
